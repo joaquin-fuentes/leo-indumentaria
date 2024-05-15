cfd02fd1cc244017a157833ba68ab6a4d76e0df2
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-05-15 13:42:43 -0300

    arregle seccion2

--- src/components/views/inicio/Seccion2.jsx
+++ src/components/views/inicio/Seccion2.jsx
@@ -7,28 +7,18 @@ const Seccion2 = () => {
     return (
         <div>
             <section className="container my-3">
-                <div
-                    className="row p-2 d-flex justify-content-center d-lg-flex justify-content-lg-around  d-md-flex justify-content-md-between">
-                    <article data-aos="fade-up" className=" cardSeccion col-11 col-md-5 col-lg-3   card text-center  mx-1 shadow p-3 mb-4  rounded ">
-                        <aside className="card-body">
-                            <MdDeliveryDining className='iconoSeccion text-success' />
-                            <h4>Envios Gratis</h4>
-                            <p>Hace tu pedido por whatsapp y te lo llevamos a tu domicilio</p>
-                        </aside>
+                <div className="row p-2 d-flex justify-content-center d-lg-flex justify-content-lg-around  d-md-flex justify-content-md-between">
+                    <article data-aos="fade-up" className=" cardSeccion">
+                        <MdDeliveryDining className='iconoSeccion text-success' />
+                        <p className='textSeccion'>Envios Gratis</p>
                     </article>
-                    <article data-aos="fade-up" className=" cardSeccion col-11 col-md-5 col-lg-3 mx-3 card text-center  mx-1 shadow p-3 mb-4  rounded">
-                        <aside className="card-body">
-                            <FaCreditCard className='iconoSeccion p-1 text-primary' />
-                            <h4>Medios de Pago</h4>
-                            <p className="m-0 p-0">Recibimos todos los medios de pago</p>
-                        </aside>
+                    <article data-aos="fade-up" className=" cardSeccion ">
+                        <FaCreditCard className='iconoSeccion p-1 text-primary' />
+                        <p className='textSeccion'>Medios de Pago</p>
                     </article>
-                    <article data-aos="fade-up" className=" cardSeccion col-11 col-md-5 col-lg-3  card text-center mx-1  shadow p-3 mb-4  rounded ">
-                        <aside className="card-body">
-                            <BiSolidOffer className='iconoSeccion text-danger' />
-                            <h4>Descuentos</h4>
-                            <p>Contas con descuentos comprando 3 o mas productos</p>
-                        </aside>
+                    <article data-aos="fade-up" className=" cardSeccion ">
+                        <BiSolidOffer className='iconoSeccion text-danger' />
+                        <p className='textSeccion'>Descuentos</p>
                     </article>
                 </div>
             </section>
