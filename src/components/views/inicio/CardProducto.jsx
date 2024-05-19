4c1c2b6b30a7c33a9cb08686e4e67dc029f6d774
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-05-19 19:13:10 -0300

    hice otro diseño de card en inicio

--- src/components/views/inicio/CardProducto.jsx
+++ src/components/views/inicio/CardProducto.jsx
@@ -7,22 +7,34 @@ import whatsapp from "../../../assets/iconos/iconoWP.svg"
 const CardProducto = ({ producto }) => {
 
     return (
-        <Col xs={6} md={4} lg={3}>
+        <Col xs={6} md={4} lg={2}>
             <Card data-aos="fade-up" className='cardContenedor'>
-                <Card.Img className='cardImagen' variant="top" src={producto.imagen} />
-                <Card.Body className="cardContenedorInfo">
-                    <Card.Title className='cardNombre'>{producto.nombrePrenda}</Card.Title>
-                    <Card.Text className='cardPrecio'>
-                        $ {producto.precio}
-                    </Card.Text>
-                    <div className='cardBotones'>
-                        <Button className='btnCard' variant="primary">Detalle</Button>
-                        <NavLink> <img src={whatsapp} alt="icono whatsapp" /></NavLink>
+                <div className="contenedorImagen">
+                    <img src={producto.imagen} alt="imagen del producto" className="cardImagen" />
+                </div>
+                <div className="contenedorInfo">
+                    <p className="cardNombre">{producto.nombrePrenda}</p>
+                    <p className="cardPrecio">${producto.precio}</p>
+                    <div className="contenedorBotones">
+                        <a href="#" className="btnCard">Ver detalle</a>
+                        <img src={whatsapp} alt="icono de whatsapp" className="iconoCard" />
                     </div>
-                </Card.Body>
+
+                </div>
             </Card>
         </Col>
     );
 };
 
 export default CardProducto;
+// <Card.Img className='cardImagen' variant="top" src={producto.imagen} />
+// <Card.Body className="cardContenedorInfo">
+//     <Card.Title className='cardNombre'>{producto.nombrePrenda}</Card.Title>
+//     <Card.Text className='cardPrecio'>
+//         $ {producto.precio}
+//     </Card.Text>
+//     <div className='cardBotones'>
+//         <Button className='btnCard' variant="primary">Detalle</Button>
+//         <NavLink> <img src={whatsapp} alt="icono whatsapp" /></NavLink>
+//     </div>
+// </Card.Body>
