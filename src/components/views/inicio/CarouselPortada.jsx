6094e7911ebd270ad7749fd3784aaf75f052336e
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-05-22 10:10:23 -0300

    cambie el banner completo

--- src/components/views/inicio/CarouselPortada.jsx
+++ src/components/views/inicio/CarouselPortada.jsx
@@ -1,6 +1,10 @@
 
 import {Carousel} from 'react-bootstrap';
 import { useState } from "react";
+import banner1 from "../../../assets/imagenes/banner1.png"
+import banner2 from "../../../assets/imagenes/banner4.png"
+import banner3 from "../../../assets/imagenes/banner5.png"
+
 
 function CarouselPortada() {  const [index, setIndex] = useState(0);
 
@@ -11,13 +15,13 @@ function CarouselPortada() {  const [index, setIndex] = useState(0);
     return (
       <Carousel className='carouselInicio' activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item className='carouselItem'>
-          <img className='imgCarousel' src="https://res.cloudinary.com/dep95zom7/image/upload/v1715687523/LeoIndumentaria/banner_tienda_abstracto_morado_y_verde_lfgeni.png" alt='imagen portada '/>
+          <img className='imgCarousel' src={banner1} alt='imagen portada '/>
         </Carousel.Item>
         <Carousel.Item className='carouselItem'>
-          <img className='imgCarousel' src="https://res.cloudinary.com/dep95zom7/image/upload/v1715687513/LeoIndumentaria/Banner_horizontal_promci%C3%B3n_2x1_restaurant_comida_saludable_org%C3%A1nico_verde_szmita.png" alt=''/>
+          <img className='imgCarousel' src={banner2} alt=''/>
         </Carousel.Item>
         <Carousel.Item className='carouselItem'>
-          <img className='imgCarousel' src="https://res.cloudinary.com/dep95zom7/image/upload/v1715688365/LeoIndumentaria/Banner_Horizontal_en_Crema_Naranja_Verde_estilo_Org%C3%A1nico_Delicado_1_eekj47.png" alt='' />
+          <img className='imgCarousel' src={banner3} alt='' />
         </Carousel.Item>
       </Carousel>
     );
