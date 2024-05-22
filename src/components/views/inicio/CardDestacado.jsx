1dc650909791a292484033776eb5e76e5b6c76ae
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-05-22 12:00:12 -0300

    cambio en cardDestacado

--- src/components/views/inicio/CardDestacado.jsx
+++ src/components/views/inicio/CardDestacado.jsx
@@ -14,11 +14,9 @@ const CardDestacado = ({ producto }) => {
                 </div>
                 <div className="contenedorInfo">
                     <p className="cardNombre">{producto.nombrePrenda}</p>
-                    <p className="cardNombre mt-0">Talle: {producto.talle}</p>
                     <p className="cardPrecio">${producto.precio}</p>
                     <div className="contenedorBotones">
                         <NavLink end to={`/detalle/${producto._id}`} className="btnCard">Ver detalle</NavLink>
-                        <Link target="_blank" to={`https://api.whatsapp.com/send?phone=3816097754&text=¡Hola! te queria consultar sobre ${producto.nombrePrenda}`}><img src={whatsapp} alt="icono de whatsapp" className="iconoCard" /></Link>
                     </div>
 
                 </div>
