725ca6bc594acecaa50095b2390539d34b9ee87d
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-05-22 10:47:06 -0300

    cambie tamaño de banner

--- src/components/views/inicio/Portada.jsx
+++ src/components/views/inicio/Portada.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import "./Inicio.css"
-import { Container} from "react-bootstrap"
 import CarouselPortada from "./CarouselPortada"
 
 const Portada = () => {
     return (
-        <div  className='contenedorPortada'>
-            <div className='contenedorImagenPortada'>
-                <CarouselPortada data-aos="fade-up"></CarouselPortada>
-            </div>
+        <div className='contenedorPortada'>
+            <CarouselPortada data-aos="fade-up"></CarouselPortada>
         </div>
     );
 };
